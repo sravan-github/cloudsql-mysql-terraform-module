74619b6b005257b269cb837e7602bc570cfe19ee
sravan-github <39382490+sravan-github@users.noreply.github.com>
2022-04-10 22:10:40 +0530

    Add files via upload

---
+++ terraform-gcp-mysql-main/example/example.tf
@@ -0,0 +1,39 @@
+resource "random_id" "suffix" {
+  byte_length = 5
+}
+
+
+module "mysql-db" {
+  source               = "../"
+  name                 = "example-mysql-private"
+  random_instance_name = true
+  project_id           = "gcp-practice-secondary-prj"
+
+  deletion_protection = false
+
+  database_version = "MYSQL_5_6"
+  region           = "us-central1"
+  zone             = "us-central1-c"
+  tier             = "db-n1-standard-1"
+
+  // By default, all users will be permitted to connect only via the
+  // Cloud SQL proxy.
+  additional_users = [
+    {
+      name     = "app"
+      password = "PaSsWoRd"
+      host     = "localhost"
+      type     = "BUILT_IN"
+    },
+    {
+      name     = "hellokoding"
+      password = "hellokoding"
+      host     = "localhost"
+      type     = "BUILT_IN"
+    },
+  ]
+}
+output "instance_connection_name" {
+  value       = module.mysql-db.instance_connection_name
+  description = "The connection name of the master instance to be used in connection strings"
+}
